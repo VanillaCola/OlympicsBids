b57be22224c815badafb8892c0d96e03d6fe5ebc
lijiyuan1992@hotmail.com <lijiyuan1992@hotmail.com>
2016-11-04 14:29:37 -0600

    Display cities on map

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
+var mapData;
 
 
 function drawMap(world)
 {
-	projection = d3.geoEquirectangular().scale(160).translate([450, 200]);
+	var projection = d3.geoEquirectangular().scale(160).translate([450, 200]);
 	
 	var map = d3.select("#map");
 	
@@ -12,7 +13,6 @@ function drawMap(world)
 	var countries = topojson.feature(world, world.objects.countries).features;
 	var lands = topojson.feature(world, world.objects.land).features;
 	var graticule = d3.geoGraticule();
-	
 		
 	map.selectAll(".countries")
 		.data(countries)
@@ -27,9 +27,66 @@ function drawMap(world)
 		
 }
 
+function clearMap()
+{
+	var map = d3.select("#map");
+	
+	map.selectAll("rect").remove();
+}
+
+function updateMap(selection)
+{
+	clearMap();
+	
+	var map = d3.select("#map");
+	
+	var projection = d3.geoEquirectangular().scale(160).translate([450, 200]);
+	
+	map.selectAll("rect")
+		.data(mapData)
+		.enter()
+		.append("rect")
+		.attr("class", function(d)
+		{
+			return d.Continents;
+		})
+		.attr("x", function(d)
+		{
+			var pos = projection(d.pos);
+			return pos[0] - (d.count_of_host+10)/2;
+		})
+		.attr("y", function(d)
+		{
+			var pos = projection(d.pos);
+			return pos[1] - (d.count_of_host+10)/2;
+		})
+		.attr("width", function(d)
+		{
+			return 10 + d.count_of_host;
+		})
+		.attr("height", function(d)
+		{
+			return 10 + d.count_of_host;
+		});
+		
+}
+
 
 d3.json("data/world-50m.json", function(error, world)
 {
 	if(error) throw error;
 	drawMap(world);
 });
+
+d3.csv("data/Map_candidate_cities_data.csv", function(error, csv)
+{
+	csv.forEach(function(d)
+	{
+		d.pos = [+d.Longitude, +d.Latitude];
+		d.count_of_host = +d["Count of Host"];
+	});
+	
+	mapData = csv;
+	updateMap("all");
+});
+
